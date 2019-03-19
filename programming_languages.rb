33f81b56a3fc16e90b1928a2ffc27a700c05af25
Adam Kearney <aklives@hotmail.com>
2019-03-19 09:15:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,10 @@
 def reformat_languages(languages)
   new_hash = {}
   languages[:oo].each do |k,v|
-   new_hash[k] = v 
+   new_hash[k] = v
    new_hash[k][:style] = [:oo]
   end
-  
+
   languages[:functional].each do |k,v|
     new_hash[k] = v
     new_hash[k][:style] = [:functional]
